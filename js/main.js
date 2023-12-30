d654b95e4678131bf6cc142d216bd05fd190ff4a
rana <ranaelrefay@gmail.com>
2023-12-30 16:11:57 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,103 @@
+var signupName = document.getElementById("signupName");
+var signupEmail = document.getElementById("signupEmail");
+var signupPassword = document.getElementById("signupPassword");
+var signinEmail = document.getElementById("signinEmail");
+var signinPassword = document.getElementById("signinPassword");
+
+// to say welcome
+var username = localStorage.getItem("sessionUsername");
+if (username) {
+  document.getElementById("username").innerHTML = "Welcome " + username;
+}
+
+var signUpArray = [];
+if (localStorage.getItem("users") == null) {
+  signUpArray = [];
+} else {
+  signUpArray = JSON.parse(localStorage.getItem("users"));
+}
+
+//check inputs is empty || not
+function isEmpty() {
+  if (
+    signupName.value == "" ||
+    signupEmail.value == "" ||
+    signupPassword.value == ""
+  ) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// for check email is exist
+function isEmailExist() {
+  for (var i = 0; i < signUpArray.length; i++) {
+    if (signUpArray[i].email.toLowerCase() == signupEmail.value.toLowerCase()) {
+      return false;
+    }
+  }
+}
+
+function signUp() {
+  if (isEmpty() == false) {
+    document.getElementById("exist").innerHTML =
+      '<span class="text-danger m-3">All inputs is required</span>';
+    return false;
+  }
+  // to store value
+  var signUp = {
+    name: signupName.value,
+    email: signupEmail.value,
+    password: signupPassword.value,
+  };
+  if (signUpArray.length == 0) {
+    signUpArray.push(signUp);
+    localStorage.setItem("users", JSON.stringify(signUpArray));
+    document.getElementById("exist").innerHTML =
+      '<span class="text-success m-3">Success</span>';
+    return true;
+  }
+  if (isEmailExist() == false) {
+    document.getElementById("exist").innerHTML =
+      '<span class="text-danger m-3">email already exists</span>';
+  } else {
+    signUpArray.push(signUp);
+    localStorage.setItem("users", JSON.stringify(signUpArray));
+    document.getElementById("exist").innerHTML =
+      '<span class="text-success m-3">Success</span>';
+  }
+}
+
+//  for login check inputs is empty || not
+function isLoginEmpty() {
+  if (signinPassword.value == "" || signinEmail.value == "") {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function login() {
+  if (isLoginEmpty() == false) {
+    document.getElementById("incorrect").innerHTML =
+      '<span class="text-danger m-3">All inputs is required</span>';
+    return false;
+  }
+  var password = signinPassword.value;
+  var email = signinEmail.value;
+  for (var i = 0; i < signUpArray.length; i++) {
+    if (
+      signUpArray[i].email.toLowerCase() == email.toLowerCase() &&
+      signUpArray[i].password.toLowerCase() == password.toLowerCase()
+    ) {
+      localStorage.setItem("sessionUsername", signUpArray[i].name);
+      window.location = "../logout.html";
+    }
+  }
+}
+
+// for logout
+function logout() {
+  localStorage.removeItem("sessionUsername");
+}
